aa9d187e17b10bde1b74d72c2f3507f4b5a50165
Adrien Verriele <adrien.verriele08@gmail.com>
2026-07-20 17:13:39 +0200

    fix: add __disable_irq and remove unused code from main.c

--- app/src/main.c
+++ app/src/main.c
@@ -3,10 +3,6 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define GPIOA_BASE 0x40020000UL
-#define GPIOA      ((GPIO_Port *)GPIOA_BASE)
-#define GPIOC      ((GPIO_Port *)(GPIOA_BASE + 0x800UL))
-
 int main(void)
 {
     uart_init(115200);
