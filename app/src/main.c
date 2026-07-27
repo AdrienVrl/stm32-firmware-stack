cd9eb3993ef590c3f71f533ec6effe45a8196055
Adrien Verriele <adrien.verriele08@gmail.com>
2026-07-27 20:06:44 +0200

    fix: clang-format

--- app/src/main.c
+++ app/src/main.c
@@ -1,36 +1,29 @@
-
 #include "pwm.h"
 #include "uart.h"
 
 #include <stdint.h>
+#include <stdio.h>
 
 int main(void)
 {
     uart_init(115200);
-
     pwm_init(1000);
-
-    pwm_set_duty(0);
-
-    for (uint8_t d = 0; d <= 100; d++)
+    while (1)
     {
-        pwm_set_duty(d);
+        for (uint8_t d = 0; d <= 100; d++)
+        {
+            pwm_set_duty(d);
+            for (volatile uint32_t i = 0; i < d; i++)
+            {
+            }
+        }
+        for (uint8_t d = 100; d > 0; d--)
+        {
+            pwm_set_duty(d - 1);
+
+            for (volatile uint32_t i = 0; i < d; i++)
+            {
+            }
+        }
     }
-        while
-
-          for (uint8_t d = 0; d <= 100; d+
-
-              pwm_set_duty(d
-              for (volatile uint32_t i = 0; i < 100000; i+
-
-
-
-          for (uint8_t d = 100; d > 0; d-
-
-              pwm_set_duty(d - 1
-              for (volatile uint32_t i = 0; i < 100000; i+
-
-
-
-      }
 }
