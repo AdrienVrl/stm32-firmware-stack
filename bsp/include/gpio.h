6af1a4c25f3991a0b93f60279a5bbb2475f86c27
Adrien Verriele <adrien.verriele08@gmail.com>
2026-07-30 19:55:21 +0200

    feat: add FreeRTOS vendor and config

--- bsp/include/gpio.h
+++ bsp/include/gpio.h
@@ -66,4 +66,6 @@ void GPIO_WritePin(GPIO_Port *port, uint8_t pin, uint8_t value);
 
 GPIO_PinState GPIO_ReadPin(GPIO_Port *port, uint8_t pin);
 
+void GPIO_TogglePin(GPIO_Port *port, uint8_t pin);
+
 #endif
